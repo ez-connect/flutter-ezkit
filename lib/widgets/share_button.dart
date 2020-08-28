0b8d57f423793860007d4ad6300a295cf6600bc5
Vinh <thanh.vinh@hotmail.com>
2020-08-28 10:24:23 +0700

    Add share button

---
+++ lib/widgets/share_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:share/share.dart';
+
+class ShareButton extends StatelessWidget {
+  final Widget icon;
+  final String text;
+  final String subject;
+
+  const ShareButton({
+    Key key,
+    this.icon,
+    this.text,
+    this.subject,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(icon: this.icon, onPressed: this._onPressed);
+  }
+
+  void _onPressed() {
+    Share.share(this.text, subject: this.subject);
+  }
+}
