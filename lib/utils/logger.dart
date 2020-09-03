b7f3f271edda3794089edba62bf6f9e3cf5ccd13
Vinh <thanh.vinh@hotmail.com>
2020-09-03 20:33:00 +0700

    Disable all log in release mode

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -1,19 +1,31 @@
 // Use ANSI escape code
 // https://en.wikipedia.org/wiki/ANSI_escape_code#3/4_bit
+import 'package:flutter/foundation.dart';
+
 class Logger {
+  static bool enabled = kDebugMode;
+
   static void debug(Object object) {
-    print('\x1b[90m ${object.toString()}');
+    if (enabled) {
+      print('\x1b[90m ${object.toString()}');
+    }
   }
 
   static void info(Object object) {
-    print('\x1b[35m ${object.toString()}');
+    if (enabled) {
+      print('\x1b[35m ${object.toString()}');
+    }
   }
 
   static void warn(Object object) {
-    print('\x1b[33m ${object.toString()}');
+    if (enabled) {
+      print('\x1b[33m ${object.toString()}');
+    }
   }
 
   static void error(Object object) {
-    print('\x1b[31m ${object.toString()}');
+    if (enabled) {
+      print('\x1b[31m ${object.toString()}');
+    }
   }
 }
